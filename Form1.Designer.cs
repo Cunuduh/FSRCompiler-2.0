c30d8aed24c2b0b6d002843866107b33bb44ba9a
Cunuduh <nickleodean.email3@gmail.com>
2022-09-24 01:12:10 -0400

    finished stuff

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,19 +39,41 @@
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox13 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 121);
+            this.textBox1.Location = new System.Drawing.Point(79, 172);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(378, 23);
             this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(190, 225);
+            this.button1.Location = new System.Drawing.Point(188, 382);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 47);
             this.button1.TabIndex = 1;
@@ -61,23 +83,23 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(87, 160);
+            this.textBox2.Location = new System.Drawing.Point(79, 211);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(378, 23);
             this.textBox2.TabIndex = 2;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "Armour",
+            "Default",
+            "Armour Model",
+            "Armour Icon",
             "Bow",
-            "Block GUI",
             "Skull",
-            "Flower",
-            "Misc Item/Sword/Tool"});
-            this.comboBox1.Location = new System.Drawing.Point(87, 81);
+            "Fishing Rod"});
+            this.comboBox1.Location = new System.Drawing.Point(79, 132);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(378, 23);
             this.comboBox1.TabIndex = 3;
@@ -86,17 +108,16 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 84);
+            this.label1.Location = new System.Drawing.Point(5, 135);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 15);
             this.label1.TabIndex = 4;
             this.label1.Text = "Item Type";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 124);
+            this.label2.Location = new System.Drawing.Point(5, 175);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(68, 15);
             this.label2.TabIndex = 5;
@@ -105,7 +126,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 163);
+            this.label3.Location = new System.Drawing.Point(5, 214);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(68, 15);
             this.label3.TabIndex = 6;
@@ -114,26 +135,28 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(79, 225);
+            this.checkBox1.Location = new System.Drawing.Point(76, 382);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(105, 19);
             this.checkBox1.TabIndex = 7;
             this.checkBox1.Text = "Use item name";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(79, 250);
+            this.checkBox2.Location = new System.Drawing.Point(76, 407);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(68, 19);
             this.checkBox2.TabIndex = 8;
             this.checkBox2.Text = "Use lore";
             this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(87, 40);
+            this.textBox3.Location = new System.Drawing.Point(79, 94);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(378, 23);
             this.textBox3.TabIndex = 9;
@@ -141,17 +164,254 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 43);
+            this.label4.Location = new System.Drawing.Point(5, 97);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 15);
+            this.label4.Size = new System.Drawing.Size(72, 15);
             this.label4.TabIndex = 10;
-            this.label4.Text = "File Name";
+            this.label4.Text = "Sprite Name";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(79, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(378, 23);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Enabled = false;
+            this.label5.Location = new System.Drawing.Point(5, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Model Path";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(337, 382);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(112, 19);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "Use custom NBT";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Enabled = false;
+            this.label6.Location = new System.Drawing.Point(6, 257);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Custom NBT";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(80, 254);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(378, 67);
+            this.textBox5.TabIndex = 14;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(75, 432);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(135, 19);
+            this.checkBox5.TabIndex = 19;
+            this.checkBox5.Text = "Custom texture path";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(337, 407);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(132, 19);
+            this.checkBox6.TabIndex = 20;
+            this.checkBox6.Text = "Custom model path";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Enabled = false;
+            this.label8.Location = new System.Drawing.Point(5, 55);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 15);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Texture Path";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(80, 52);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(378, 23);
+            this.textBox7.TabIndex = 21;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Enabled = false;
+            this.label7.Location = new System.Drawing.Point(12, 342);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 15);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Lore";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(80, 339);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(378, 23);
+            this.textBox6.TabIndex = 23;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Enabled = false;
+            this.textBox8.Location = new System.Drawing.Point(468, 403);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(119, 23);
+            this.textBox8.TabIndex = 25;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Enabled = false;
+            this.textBox9.Location = new System.Drawing.Point(468, 132);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(119, 23);
+            this.textBox9.TabIndex = 26;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Enabled = false;
+            this.label9.Location = new System.Drawing.Point(484, 114);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 15);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Skull";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Enabled = false;
+            this.label10.Location = new System.Drawing.Point(468, 386);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(115, 15);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "Leather Overlay Icon";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Enabled = false;
+            this.textBox10.Location = new System.Drawing.Point(468, 449);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(119, 23);
+            this.textBox10.TabIndex = 29;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Enabled = false;
+            this.label11.Location = new System.Drawing.Point(484, 432);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(88, 15);
+            this.label11.TabIndex = 30;
+            this.label11.Text = "Armour Layer 2";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Enabled = false;
+            this.label12.Location = new System.Drawing.Point(603, 432);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(87, 15);
+            this.label12.TabIndex = 34;
+            this.label12.Text = "Overlay Layer 2";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Enabled = false;
+            this.textBox11.Location = new System.Drawing.Point(593, 449);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(119, 23);
+            this.textBox11.TabIndex = 33;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Enabled = false;
+            this.label13.Location = new System.Drawing.Point(609, 386);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(87, 15);
+            this.label13.TabIndex = 32;
+            this.label13.Text = "Overlay Layer 1";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Enabled = false;
+            this.textBox12.Location = new System.Drawing.Point(593, 403);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(119, 23);
+            this.textBox12.TabIndex = 31;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Enabled = false;
+            this.label14.Location = new System.Drawing.Point(603, 114);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(103, 15);
+            this.label14.TabIndex = 36;
+            this.label14.Text = "Armour Material 2";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Enabled = false;
+            this.textBox13.Location = new System.Drawing.Point(593, 132);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(119, 23);
+            this.textBox13.TabIndex = 35;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 283);
+            this.ClientSize = new System.Drawing.Size(730, 484);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.checkBox2);
@@ -164,7 +424,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FSR Compiler 2.0";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,5 +443,28 @@
         private CheckBox checkBox2;
         private TextBox textBox3;
         private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private CheckBox checkBox3;
+        private Label label6;
+        private TextBox textBox5;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private Label label8;
+        private TextBox textBox7;
+        private Label label7;
+        private TextBox textBox6;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox10;
+        private Label label11;
+        private Label label12;
+        private TextBox textBox11;
+        private Label label13;
+        private TextBox textBox12;
+        private Label label14;
+        private TextBox textBox13;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,38 +1,180 @@
+using System.Text.RegularExpressions;
 namespace FSRCompiler_2._0
 {
     public partial class Form1 : Form
     {
-        public string? vanillaItem;
+        public enum ItemType
+        {
+            Default,
+            ArmourModel,
+            ArmourIcon,
+            Bow,
+            Skull,
+            FishingRod
+        }
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e) // Vanilla item
-        {
-            TextBox objTextBox = (TextBox)sender;
-            vanillaItem = objTextBox.Text;
-        }
-        private void textBox2_TextChanged(object sender, EventArgs e) // Skyblock ID
-        {
-
-        }
         private void button1_Click(object sender, EventArgs e) // Create properties file
         {
-           File.CreateText(string.Concat("item=", textBox1.Text));
+            SaveFileDialog sfd = new SaveFileDialog()
+            {
+                InitialDirectory = Application.StartupPath + "\\Scripts\\",
+                Title = "Save CIT File",
+                CheckPathExists = true,
+                DefaultExt = "properties",
+                FileName = textBox3.Text,
+                Filter = ".properties files (*.properties)|*.properties|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, CreatePropertiesText());
+            }
         }
-        private void CreatePropertiesFile(string vanillaItem, string skyblockID,)
+        private string CreatePropertiesText()
         {
-
+            string file;
+            switch (comboBox1.SelectedIndex)
+            {
+                case (int)ItemType.Bow:
+                    file = string.Concat("items=minecraft:bow",
+                                         "\ntexture.bow_standby=", textBox3.Text,
+                                         "\ntexture.bow_pulling_0=", textBox3.Text, "_pulling_0",
+                                         "\ntexture.bow_pulling_1=", textBox3.Text, "_pulling_1",
+                                         "\ntexture.bow_pulling_2=", textBox3.Text, "_pulling_2");
+                    break;
+                case (int)ItemType.ArmourIcon:
+                    file = string.Concat("items=minecraft:", textBox1.Text,
+                                         "\ntexture.", textBox1.Text, "=", textBox3.Text,
+                                         textBox1.Text.Contains("leather") ? string.Concat("\ntexture.", textBox1.Text, "_overlay=", textBox8.Text) : null);
+                    break;
+                case (int)ItemType.ArmourModel:
+                    file = string.Concat("type=armor\nitems=minecraft:", textBox1.Text,
+                                         "\ntexture.", textBox9.Text, "_layer_1=", textBox8.Text,
+                                         textBox9.Text == "leather" ? string.Concat("\ntexture.", textBox9.Text, "layer_1_overlay=", textBox12.Text) : null,
+                                         "\ntexture.", textBox9.Text, "_layer_2=", textBox10.Text,
+                                         textBox9.Text == "leather" ? string.Concat("\ntexture.", textBox9.Text, "layer_2_overlay=", textBox11.Text) : null);
+                                        
+                    if (Regex.IsMatch(textBox13.Text, "[a-zA-Z]"))
+                    {
+                        file = string.Concat(file, "\ntexture.", textBox13.Text, "_layer_1=", textBox8.Text,
+                                             textBox13.Text == "leather" ? string.Concat("\ntexture.", textBox13.Text, "layer_1_overlay=", textBox12.Text) : null,
+                                             "\ntexture.", textBox13.Text, "_layer_2=", textBox10.Text,
+                                             textBox13.Text == "leather" ? string.Concat("\ntexture.", textBox13.Text, "layer_2_overlay=", textBox11.Text) : null);
+                    }
+                    break;
+                case (int)ItemType.Skull:
+                    file = string.Concat("nbt.SkullOwner.Properties.textures.0.Value=", textBox9);
+                    break;
+                default:
+                    file = string.Concat("items=minecraft:", textBox1.Text);
+                    break;
+            }
+            return string.Concat(file,
+                                 checkBox6.Checked ? string.Concat("\nmodel=", textBox4.Text) : null,
+                                 checkBox5.Checked ? string.Concat("\ntexture=", textBox3.Text) : null,
+                                 checkBox1.Checked ? string.Concat("\nnbt.display.Name=ipattern:", textBox2.Text) : string.Concat("\nnbt.ExtraAttributes.id=", textBox2.Text),
+                                 checkBox3.Checked ? string.Concat("\n", textBox5.Text) : null,
+                                 checkBox2.Checked ? string.Concat("\nnbt.display.Lore.*=ipattern:", textBox6.Text) : null);
         }
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if (comboBox1.SelectedIndex == (int)ItemType.ArmourIcon)
+            {
+                label9.Text = "Skull";
+                label10.Text = "Leather Overlay Icon";
+                label10.Enabled = true;
+                textBox8.Enabled = true;
+                label9.Enabled = false;
+                textBox9.Enabled = false;
+                label11.Enabled = false;
+                textBox10.Enabled = false;
+                label14.Enabled = false;
+                textBox13.Enabled = false;
+                return;
+            }
+            if (comboBox1.SelectedIndex == (int)ItemType.ArmourModel)
+            {
+                label9.Text = "Armour Material";
+                label10.Text = "Armour Layer 1";
+                label9.Enabled = true;
+                textBox9.Enabled = true;
+                label10.Enabled = true;
+                textBox8.Enabled = true;
+                label11.Enabled = true;
+                textBox10.Enabled = true;
+                label14.Enabled = true;
+                textBox13.Enabled = true;
+                return;
+            }
+            label9.Text = "Skull";
+            label10.Text = "Armour Layer 1";
+            label10.Enabled = false;
+            textBox8.Enabled = false;
+            label9.Enabled = false;
+            textBox9.Enabled = false;
+            label11.Enabled = false;
+            textBox10.Enabled = false;
+            label14.Enabled = false;
+            textBox13.Enabled = false;
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                label3.Text = "Item Name";
+                return;
+            }
+            label3.Text = "Skyblock ID";
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                label7.Enabled = true;
+                textBox6.Enabled = true;
+                return;
+            }
+            label7.Enabled = false;
+            textBox6.Enabled = false;
+        }
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                label6.Enabled = true;
+                textBox5.Enabled = true;
+                return;
+            }
+            label6.Enabled = false;
+            textBox5.Enabled = false;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
         {
+            if (checkBox5.Checked)
+            {
+                label8.Enabled = true;
+                textBox7.Enabled = true;
+                return;
+            }
+            label8.Enabled = false;
+            textBox7.Enabled = false;
+        }
 
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox6.Checked)
+            {
+                label5.Enabled = true;
+                textBox4.Enabled = true;
+                return;
+            }
+            label5.Enabled = false;
+            textBox4.Enabled = false;
         }
     }
-}
+} // if ur reading this i <3 u
